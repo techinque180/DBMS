cf1b90428fb70f8271d76ab79993ac7af7aa68bc
yuhan1999 <D0713019@o365.fcu.edu.tw>
2020-06-07 16:46:39 +0800

    commit

---
+++ app/src/main/java/com/example/myproject/BalanceData.java
@@ -0,0 +1,13 @@
+package com.example.myproject;
+
+public class BalanceData {
+    private String balance_money;
+
+    public String getBalance_money() {
+        return balance_money;
+    }
+
+    public void setBalance_money(String balance_money) {
+        this.balance_money = balance_money;
+    }
+}
--- app/src/main/java/com/example/myproject/resident.java
+++ app/src/main/java/com/example/myproject/resident.java
@@ -9,6 +9,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class resident extends AppCompatActivity {
 
     private Button bnt_record;
@@ -17,6 +26,7 @@ public class resident extends AppCompatActivity {
     private Button bnt_resusecondition;
     private Button bnt_resdamagelevel;
     private String account;
+    private String result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +40,9 @@ public class resident extends AppCompatActivity {
         bnt_ressearchbalance = (Button) findViewById(R.id.bnt_ressearchbalance);
         bnt_resusecondition = (Button) findViewById(R.id.bnt_resusecondition);
         bnt_resdamagelevel = (Button) findViewById(R.id.bnt_resdamagelevel);
+
+        Thread thread = new Thread(mutiThread);
+        thread.start();
 
         bnt_record.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,4 +96,61 @@ public class resident extends AppCompatActivity {
             }
         });
     }
+
+    private Runnable mutiThread = new Runnable() {
+        @Override
+        public void run() {
+            try {
+//                10.22.15.106
+                URL url = new URL("http://10.22.15.106/account_connect/get_all_account.php");
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.setUseCaches(false);
+                connection.connect();
+
+                int responseCode = connection.getResponseCode();
+
+                if(responseCode == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader bufReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String box = "";
+                    String line = null;
+                    while ((line = bufReader.readLine()) != null ) {
+                        box += line + '\n';
+                    }
+                    inputStream.close();
+                    result = box;
+                }
+                // 讀取輸入串流並存到字串的部分
+                // 取得資料後想用不同的格式
+                // 例如 Json 等等，都是在這一段做處理
+                JSONArray array = new JSONArray(result);
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject jsonObject = array.getJSONObject(i);
+                    BalanceData balanceData = new BalanceData();
+                    String room_no = jsonObject.getString("room_no");
+                    if(account.equals(room_no)) {
+                        balanceData.setBalance_money(jsonObject.getString("balance_money"));
+                        System.out.println("money is" + balanceData.getBalance_money());
+                    }
+//                    System.out.println(accountDatas[i].getAccount_id() + " " + accountDatas[i].getPassword() + " "
+//                            + accountDatas[i].getManager_id() + " " + accountDatas[i].getBalance_money());
+                    //Log.d("TAG", "title:" + title + ", tag:" + tag + ", info:" + info);
+                }
+
+
+            } catch (Exception e) {
+                result = e.toString();
+            }
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    //textView.setText(result);
+                    System.out.println(result); // 更改顯示文字
+                }
+            });
+        }
+    };
 }
